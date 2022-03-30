ff1ae1f0db6f40646e2ac275ee0e996df505a061
CarlosMacaneta <carlosmacaneta@hotmail.com>
2022-03-30 09:04:50 +0200

    Handling location permissions

--- app/src/main/java/com/cs/runningappyt/ui/fragments/RunFragment.kt
+++ app/src/main/java/com/cs/runningappyt/ui/fragments/RunFragment.kt
@@ -1,5 +1,7 @@
 package com.cs.runningappyt.ui.fragments
 
+import android.Manifest
+import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -10,10 +12,14 @@ import androidx.navigation.fragment.findNavController
 import com.cs.runningappyt.R
 import com.cs.runningappyt.databinding.FragmentRunBinding
 import com.cs.runningappyt.ui.viewmodels.MainViewModel
+import com.cs.runningappyt.utils.Constants.REQUEST_CODE_LOCATION_PERMISSION
+import com.cs.runningappyt.utils.TrackingUtility
 import dagger.hilt.android.AndroidEntryPoint
+import pub.devrel.easypermissions.AppSettingsDialog
+import pub.devrel.easypermissions.EasyPermissions
 
 @AndroidEntryPoint
-class RunFragment: Fragment() {
+class RunFragment: Fragment(), EasyPermissions.PermissionCallbacks {
 
     private lateinit var binding: FragmentRunBinding
 
@@ -31,10 +37,54 @@ class RunFragment: Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        requestPermissions()
+
         binding.fab.setOnClickListener {
             findNavController().navigate(R.id.action_runFragment_to_trackingFragment)
         }
     }
 
+    private fun requestPermissions() {
+        if (TrackingUtility.hasLocationPermissions(requireContext())) {
+            return
+        }
 
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.Q) {
+            EasyPermissions.requestPermissions(
+                this,
+                "You must accept location permissions to use this app",
+                REQUEST_CODE_LOCATION_PERMISSION,
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            )
+        } else {
+            EasyPermissions.requestPermissions(
+                this,
+                "You must accept location permissions to use this app",
+                REQUEST_CODE_LOCATION_PERMISSION,
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_BACKGROUND_LOCATION
+            )
+        }
+    }
+
+    override fun onPermissionsGranted(requestCode: Int, perms: MutableList<String>) {}
+
+    override fun onPermissionsDenied(requestCode: Int, perms: MutableList<String>) {
+        if (EasyPermissions.somePermissionPermanentlyDenied(this, perms)) {
+            AppSettingsDialog.Builder(this).build().show()
+        } else {
+            requestPermissions()
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        EasyPermissions.onRequestPermissionsResult(requestCode, permissions, grantResults, this)
+    }
 }
---
+++ app/src/main/java/com/cs/runningappyt/utils/Constants.kt
@@ -0,0 +1,6 @@
+package com.cs.runningappyt.utils
+
+object Constants {
+
+    const val REQUEST_CODE_LOCATION_PERMISSION = 0
+}
